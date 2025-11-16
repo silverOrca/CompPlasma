01b8a722b3adedb8bae1646363cb7c5725529c60
Ciara <ciaramalcolm@outlook.com>
2025-11-16 14:57:52 +0000

    started portfolio 1

---
+++ Portfolios/Portfolio 1/4 - implementation.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 16 11:22:43 2025
+
+@author: ciara
+
+Need to solve equations of motion for a single particle in the x and y directions
+"""
+
+import matplotlib.pyplot as plt
+from numpy import zeros, linspace
+from scipy.integrate import solve_ivp
+ 
+
+#define how many spaces I want in x and y
+nx = 1001 ; ny = 501 
+#define length of x and y
+L_x = 100.0 ; L_y = 50.0
+
+#set out the individual points along x and y
+xx = linspace(0, L_x, nx)
+yy = linspace(0, L_y, ny)
+
+#define an empty array of spatial dimensions to get B for each spatial location
+Bmag = zeros([nx, ny])
+
+#define initial parameters
+x_0 = L_x / 2
+x_c =-L_x
+B_0 = 1.0
+
+#define the time limits
+time = linspace(0, 500, 1000)
+
+#values for each parameter to iterate over
+initial_x = [3*L_x/4, L_x/2, L_x/4, L_x/2]
+initial_y = [L_y/2, L_y/2, L_y/2, L_y/2]
+initial_vx = [1.0, 1.0, 1.0, 1.0]
+initial_vy = [0.0, 0.0, 0.0, 0.0]
+q = [1.0, -1.0, 1.0, 1.0]
+m = [1.0, 1.0, 0.1, 0.1]
+
+
+
+#function defining the equation of motion of charged particle
+#changing variables: v_x, v_y, B_z, 
+def dvdt(curT, curF, q, m, E_x=0, E_y=0):
+    
+    #set out the variables
+    x = curF[0]
+    y = curF[1]
+    v_x = curF[2]
+    v_y = curF[3]
+    
+    dx_dt = v_x
+    dy_dt = v_y
+    
+    #need to find what the current B_z is
+    B_z = B_func(x, y, x_0, x_c, B_0)
+    
+    
+    dvx_dt = (q/m) * (v_y * B_z + E_x)
+    
+    dvy_dt = (q/m) * ((-v_x * B_z) + E_y)
+    
+    
+    return [dx_dt, dy_dt, dvx_dt, dvy_dt]
+    
+    
+    
+    
+
+
+
+#example of a magnetic field varying with position
+def B_func(x, y, x_0, x_c, B_0):
+
+    #doesn't use y position in equation
+    B_z = (B_0 * x_0) / (x - x_c)
+    
+    Bmag[x, y] = B_z
+    
+    return B_z
+
+
+#to plot anything needed
+def plotting():
+    
+    #create the plot      
+    plt.pcolormesh(xx, yy, Bmag.T, shading = 'auto')
+    plt.xlabel(r'$\hat{x}$, normalised to thermal Hydrogen gyro-radius, $\rho_H$', fontsize=8) 
+    plt.ylabel(r'$\hat{y}$, normalised to thermal Hydrogen gyro-period, $\tau_H$', fontsize=8)
+    #plt.title(r"")
+    plt.text(-4, -11, r'Figure 1: Example $B_{func}$ output showing $B_z$ varying with $\hat{x}$ and $\hat{y}$.')
+    plt.colorbar()
+
+
+
+#runs the code to output the B_func plot
+def run_B_plot(x, y):
+    
+    #fill the empty array with spatially varying B calculated from B_func
+    for ix, x in enumerate(xx):
+        for iy, y in enumerate(yy):
+            Bmag[ix, iy] = B_func(x, y, x_0, x_c, B_0)
+            
+    return Bmag
+    #plotting()
+
+
+    
+def main():
+    E_x = 0.0 
+    E_y = 0.0
+    for i in range(len(initial_x)):
+        f0 = [initial_x[i], initial_y[i], initial_vx[i], initial_vy[i]]
+        solution = solve_ivp(dvdt, [time[0], time[-1]], f0, t_eval=time, args=(q[i], m[i], E_x, E_y))
+        
+        dvx_dt = solution.y[0,:]
+        dvy_dt = solution.y[1,:]
+        
+        plt.plot(time, dvx_dt, label='dvx_dt '+str(i))
+        plt.plot(time, dvy_dt, label='dvy_dt '+str(i))
+
+    plotting()    
+    
+    plt.xlabel('Time')
+    plt.ylabel('Velocity components')
+    plt.title('Velocity components of charged particle over time in varying B field')
+    plt.legend(loc='best')
+    plt.show()
+    
+    
+if __name__ == "__main__":
+    main()
